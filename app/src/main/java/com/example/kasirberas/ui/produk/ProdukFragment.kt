66d18848d98da73d589f8b3458abce6927aed467
MochCahyahadiF01 <mochcahyahadif@gmail.com>
2025-08-06 18:57:47 +0700

    first commit

---
+++ app/src/main/java/com/example/kasirberas/ui/produk/ProdukFragment.kt
@@ -0,0 +1,42 @@
+package com.example.kasirberas.ui.produk
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.kasirberas.databinding.FragmentProdukBinding
+
+class ProdukFragment : Fragment() {
+
+    private var _binding: FragmentProdukBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentProdukBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
